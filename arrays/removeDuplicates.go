9f9ace480136e0e3df35e23facf27e6fd989cc4c
Hamed Mahmoudkhani <ainyava@gmail.com>
2024-03-01 18:51:44 +0330

    Changed: improve remove duplicates

--- arrays/removeDuplicates.go
+++ arrays/removeDuplicates.go
@@ -5,20 +5,17 @@ import (
 )
 
 func removeDuplicates(nums []int) int {
-	i := 0
-	for i = range nums {
-		j := i
-		for ; j < len(nums) && nums[j] == nums[i]; j++ {
-		}
-		for k := 0; k+j < len(nums); k++ {
-			nums[i+1+k] = nums[j+k]
-		}
-		if i > 0 && nums[i] <= nums[i-1] {
-			i--
-			break
+	if len(nums) == 0 {
+		return 0
+	}
+	k := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[k] = nums[i]
+			k++
 		}
 	}
-	return i + 1
+	return k
 }
 
 func main() {
