db39ca504a31eeee02c0076b7c194745838a3b8d
Hamed Mahmoudkhani <ainyava@gmail.com>
2024-03-14 22:48:17 +0330

    Added: tree level order traverse

---
+++ trees/levelOrder.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	res := [][]int{}
+	if root == nil {
+		return res
+	}
+
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		level := []int{}
+		for _, node := range queue {
+			queue = queue[1:]
+			level = append(level, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		res = append(res, level)
+	}
+	return res
+}
+
+func main() {
+	root := TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Right: &TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &TreeNode{
+			Val: 2,
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+	}
+	fmt.Println("Level order traverse of tree", levelOrder(&root))
+}
